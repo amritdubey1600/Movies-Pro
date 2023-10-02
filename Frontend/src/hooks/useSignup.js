c0f67ec3e8be6dc26bc33e39ec307e4aedbd224b
amritdubey1600 <115389809+amritdubey1600@users.noreply.github.com>
2023-10-02 09:07:16 +0530

    Update useSignup.js

--- Frontend/src/hooks/useSignup.js
+++ Frontend/src/hooks/useSignup.js
@@ -13,7 +13,7 @@ export const useSignup = () =>{
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('http://localhost:4000/api/user/signup',{
+        const response = await fetch('https://movies-pro-1qpo.onrender.com/api/user/signup',{
             method:'POST',
             headers:{'Content-Type':'application/json'},
             body: JSON.stringify({email,password})
